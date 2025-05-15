abfa75c0816c2612f90d369298e94c174d668418
WintBit <wintbit@gmail.com>
2025-05-16 00:54:12 +0800

    fix: import

--- recorder/internal/svc/context.go
+++ recorder/internal/svc/context.go
@@ -6,8 +6,6 @@ import (
 
 	"github.com/zeromicro/go-queue/natsq"
 	"github.com/zeromicro/go-zero/core/logx"
-
-	"github.com/zeromicro/go-queue/kq"
 	"resty.dev/v3"
 	"scutbot.cn/web/rm-monitor/recorder/internal/config"
 	"scutbot.cn/web/rm-monitor/recorder/internal/record"
